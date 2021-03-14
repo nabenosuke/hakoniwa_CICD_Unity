de0c3645f4a64db7f362abbdeb771f6ab0326edf
nabenosuke <44940561+nabenosuke@users.noreply.github.com>
2021-03-15 00:36:11 +0900

    CICD

---
+++ HakoniwaML2/Assets/Scripts/CICD/Reset.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using System.Linq;
+
+namespace CICD
+{
+  public class Reset : MonoBehaviour
+  {
+
+
+    private GameObject[] robots;
+    private GameObject[] envs;
+    private GameObject stateManager;
+    private Vector3[] robotsTransform;
+    private Vector3[] envsTransform;
+    private Quaternion[] robotsQuaternion;
+    private Quaternion[] envsQuaternion;
+    private Hakoniwa.GUI.SimStartCICD simStart;
+    private int robotsNum;
+    private int envsNum;
+    // Start is called before the first frame update
+    public void Initialize()
+    {
+      stateManager = GameObject.Find("env/StateManager");
+      simStart = stateManager.GetComponent<Hakoniwa.GUI.SimStartCICD>();
+      if (stateManager == null)
+      {
+        UnityEngine.Debug.Log("StateManagerが取得できませんでした");
+      }
+      RobotsInit();
+      EnvsInit();
+
+    }
+
+    private void RobotsInit()
+    {
+      robots = GameObject.Find("Robot").GetComponentsInChildren<Transform>().Select(t => t.gameObject).ToArray();
+      robotsNum = robots.Length;
+      robotsTransform = new Vector3[robotsNum];
+      robotsQuaternion = new Quaternion[robotsNum];
+
+      int index = 0;
+      foreach (var parts in robots)
+      {
+        robotsTransform[index] = robots[index].transform.localPosition;
+        robotsQuaternion[index] = robots[index].transform.localRotation;
+        index++;
+      }
+
+    }
+
+    private void EnvsInit()
+    {
+      envs = GameObject.Find("env").GetComponentsInChildren<Transform>().Select(t => t.gameObject).ToArray();
+      envsNum = envs.Length;
+      envsTransform = new Vector3[envsNum];
+      envsQuaternion = new Quaternion[envsNum];
+
+      int index = 0;
+      foreach (var parts in envs)
+      {
+        envsTransform[index] = envs[index].transform.localPosition;
+        envsQuaternion[index] = envs[index].transform.localRotation;
+        index++;
+      }
+
+    }
+
+    private void RobotsReset()
+    {
+      int index = 0;
+      foreach (var parts in robots)
+      {
+        robots[index].transform.localPosition = robotsTransform[index];
+        robots[index].transform.localRotation = robotsQuaternion[index];
+        index++;
+      }
+    }
+
+    private void EnvsReset()
+    {
+      int index = 0;
+      foreach (var parts in envs)
+      {
+        envs[index].transform.localPosition = envsTransform[index];
+        envs[index].transform.localRotation = envsQuaternion[index];
+        index++;
+      }
+    }
+
+    public void AppReset()
+    {
+      RobotsReset();
+      EnvsInit();
+      stateManager.GetComponent<StateManager>().Initialize();
+      simStart.CloseSim();
+      //simStart.StartAthrill();
+      //UnityEngine.Debug.Log("Reset");
+      /* 再起動
+          System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+          Application.Quit();
+          */
+    }
+
+    public void StartAthrill()
+    {
+
+      simStart.StartAthrill();
+    }
+
+  }
+}
---
+++ HakoniwaML2/Assets/Scripts/CICD/SimStartCICD.cs
@@ -0,0 +1,146 @@
+﻿using Hakoniwa.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+namespace Hakoniwa.GUI
+{
+  //デバッグモードで実行できるように
+  public class SimStartCICD : MonoBehaviour
+  {
+    private Process exProcess = new Process();
+    private bool isInitialized = false;
+    private string terminal = null;
+    private string athrillPath = null;
+    HakoniwaConfig hakoniwa_cfg = null;
+
+    [SerializeField] private bool isDebug = false;
+
+    public void Initialize()
+    {
+      GameObject hakoniwa = GameObject.Find("Hakoniwa");
+      hakoniwa_cfg = hakoniwa.GetComponentInChildren<Hakoniwa.Core.HakoniwaConfig>();
+      if (hakoniwa_cfg == null)
+      {
+        UnityEngine.Debug.LogError("Not found hakoniwa_cfg : " + hakoniwa_cfg);
+        return;
+      }
+      HakoniwaConfigInfo cfg = hakoniwa_cfg.GetConfig();
+      if (cfg == null)
+      {
+        UnityEngine.Debug.LogError("Not found cfg : " + cfg);
+        return;
+      }
+
+      this.terminal = cfg.TerminalPath;
+      this.athrillPath = cfg.AthrillPath;
+    }
+
+    private void activate_one(HakoniwaRobotConfigInfo robot_cfg)
+    {
+      string arg = null;
+      if (isDebug == false)
+      {
+        if (this.hakoniwa_cfg.IsMmap(robot_cfg))
+        {
+
+          arg = string.Format("{0} -c1 -t -1 -m {1}/{2}/memory_mmap.txt  -d {1}/{2}/device_config_mmap.txt {1}/{3} ",
+              athrillPath,
+              robot_cfg.WorkspacePathUnix,
+              robot_cfg.ApplicationName,
+              robot_cfg.BinaryName);
+        }
+        else
+        {
+          arg = string.Format("{0} -c1 -t -1 -m {1}/{2}/memory.txt  -d {1}/{2}/device_config.txt {1}/{3} ",
+              athrillPath,
+              robot_cfg.WorkspacePathUnix,
+              robot_cfg.ApplicationName,
+              robot_cfg.BinaryName);
+        }
+      }
+      else
+      {
+        UnityEngine.Debug.Log("Debug Mode");
+        if (this.hakoniwa_cfg.IsMmap(robot_cfg))
+        {
+
+          arg = string.Format("{0} -c1 -i -t -1 -m {1}/{2}/memory_mmap.txt  -d {1}/{2}/device_config_mmap.txt {1}/{3} ",
+              athrillPath,
+              robot_cfg.WorkspacePathUnix,
+              robot_cfg.ApplicationName,
+              robot_cfg.BinaryName);
+        }
+        else
+        {
+          arg = string.Format("{0} -c1 -i -t -1 -m {1}/{2}/memory.txt  -d {1}/{2}/device_config.txt {1}/{3} ",
+              athrillPath,
+              robot_cfg.WorkspacePathUnix,
+              robot_cfg.ApplicationName,
+              robot_cfg.BinaryName);
+        }
+      }
+      if (this.terminal.Contains("wsl"))
+      {
+        //UnityEngine.Debug.Log(terminal);
+        exProcess.StartInfo.FileName = this.terminal;
+        exProcess.StartInfo.Arguments = arg;
+      }
+      else if (this.terminal.Contains("open"))
+      {
+        string exec_cmd = hakoniwa_cfg.GetCurrentPath() + "/athrill.command";
+        StreamWriter writer = new StreamWriter(exec_cmd);
+        writer.Write(arg);
+        writer.Close();
+        exProcess.StartInfo.FileName = "/usr/bin/open";
+        exProcess.StartInfo.Arguments = exec_cmd;
+        //exProcess = Process.Start("/usr/bin/open", exec_cmd);
+        UnityEngine.Debug.Log("exec= " + exec_cmd);
+      }
+      exProcess.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+      exProcess.Start();
+      isInitialized = true;
+      //UnityEngine.Debug.Log("arg=" + arg);
+      //UnityEngine.Debug.Log("exProcess=" + exProcess);
+    }
+
+    public void OnButtonClick()
+    {
+      //UnityEngine.Debug.Log("Button clicked");
+      if (isInitialized == false)
+      {
+        this.Initialize();
+        GameObject root = GameObject.Find("Robot");
+        foreach (Transform child in root.transform)
+        {
+          HakoniwaRobotConfigInfo robot_cfg = hakoniwa_cfg.GetRobotConfig(child.name);
+          if (robot_cfg != null)
+          {
+            this.activate_one(robot_cfg);
+          }
+        }
+      }
+    }
+
+
+    //cicd
+    //複数のプロセスを閉じれない？
+    public void CloseSim()
+    {
+      if (isInitialized == true)
+      {
+        exProcess.CloseMainWindow();
+        exProcess.Close();
+      }
+    }
+
+    public void StartAthrill()
+    {
+      isInitialized = false;
+      //UnityEngine.Debug.Log("Start Athrill");
+      OnButtonClick();
+    }
+  }
+}
